3070e307b35995ebbeac184c753b63be27f4c8c1
mudit mahajan <muditmhjn99@gmail.com>
2020-06-16 04:02:08 +0530

    json response added

--- encoder/views.py
+++ encoder/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .forms import image_upload_form
 from .convert import base64_md5
+from django.conf import settings
 # Create your views here.
 
 def image_upload(request):
@@ -9,9 +10,9 @@ def image_upload(request):
         form = image_upload_form(request.POST, request.FILES)
 
         if form.is_valid():
-            #print(form)
             form.save()
-            my_encode = convert_image(form)
+            image = request.FILES['image_main']
+            my_encode = convert_image(image)
             return JsonResponse(my_encode)
             #return render(request, 'result.html', my_encode)
 
@@ -19,9 +20,14 @@ def image_upload(request):
         form = image_upload_form()
     return render(request, 'upload.html', {'form':form})
 
-def convert_image(form):
-    image_tag = form.cleaned_data['image_tag']
-    image = form.cleaned_data['image_main']
-    d = base64_md5(image) #This is causing error because not passing image as binary.
-    # # TODO: convert image to binary
-    return d
+def convert_image(image):
+    '''
+    This function takes the name of image from form and joins it with the
+    path of the media\images folder with image. The raw string of the image
+    address is read as binary ('rb') and is passed to base64_md5 function.
+    '''
+    image_location = settings.BASE_DIR + '\media\images\{}'.format(image)
+    #with open(image_location, 'rb') as img:
+    img = open(r'''{}'''.format(image_location), 'rb')
+    encryts = base64_md5(img)
+    return encryts
